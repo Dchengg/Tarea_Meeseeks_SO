b5283d2a0bba57457f8e5fcdcee244f3cc1fb561
Diego Cheng <diegocheng1973@gmail.com>
2021-04-24 14:16:37 -0600

    semaphores for every shared variable and pipe

--- Meeseeks/box.c
+++ Meeseeks/box.c
@@ -15,7 +15,7 @@
 #define SEM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 #define PAGESIZE 4096
 
-int LEVEL_LIMIT = 7;
+int LEVEL_LIMIT = 3;
 
 int original_process;
 
@@ -27,18 +27,23 @@ char readMessage2[20];  //Recibe el mensaje del segundo pipe(Solicitud)
 
 //Variables globales compartidas
 u_int8_t *shared_instances;
-u_int8_t *shared_levels;
 u_int8_t *isFinished;
 
-//semaforo
-sem_t sem;
+//semaforos
+sem_t sem_shared_instances;
+sem_t sem_isFinished;
+sem_t sem_pipefds;
+sem_t sem_pipefds2;
 
 clock_t tic;
 clock_t toc;
 
 void initSharedVariables(){         //inicia las variables compartidas, utilizando un mmap, para asignarles un espacio de memoria a cada una
     //setup semaforos
-    sem_init(&sem, 0, 1);
+    sem_init(&sem_shared_instances, 0, 1);
+    sem_init(&sem_isFinished, 0, 1);
+    sem_init(&sem_pipefds, 0, 1);
+    sem_init(&sem_pipefds2, 0, 1);
     //setup variable compartida de las instancias
     shared_instances = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *shared_instances = 1;
@@ -61,37 +66,46 @@ int initPipe(){                     //Inicializa los dos pipes que comunicaran a
 
 int addInstance(){                  //Agrega una instancia a las instancias totales del programa
     int instances;
-    sem_wait(&sem);   //Espera a poder acceder a la variable compartida
+    sem_wait(&sem_shared_instances);   //Espera a poder acceder a la variable compartida
     instances = *shared_instances + 1;
     *shared_instances = instances;  //aumenta la variable
-    sem_post(&sem);   //Libera la variable, para que otro proceso la pueda accedar
+    sem_post(&sem_shared_instances);   //Libera la variable, para que otro proceso la pueda accedar
     return instances;   
 }
 
 int getDifficulty(){                //Consigue la difficultad pasada por el primer pipe (pipefds)
-    sem_wait(&sem);
+    sem_wait(&sem_pipefds);
     read(pipefds[0],  &readMessage, sizeof(readMessage));   //Lee el mensaje del pipe
     int difficulty = readMessage;      
     if(readMessage > 0){
         difficulty = readMessage + 1;   //si la dificultad no es 0, la aumenta 1 por cada meeseeks que accede a la dificultad
     }
     write(pipefds[1],  &readMessage, sizeof(difficulty));   //escribe la nueva dificultad para que sea accedida por el proximo meeseeks
-    sem_post(&sem);
+    sem_post(&sem_pipefds);
     return difficulty;
 }
 
 char* getRequest(){                 //Consigue la solicitud pasada por el segundo pipe
-    sem_wait(&sem);
+    sem_wait(&sem_pipefds2);
     read(pipefds2[0],  &readMessage2, sizeof(readMessage2));
     char* request = readMessage2;
-    sem_post(&sem);
+    sem_post(&sem_pipefds2);
     return request;
 }
 
 void informFinish(){                //Si un meeseeks termina, utiliza la variable compartida como flag para informar a los otros meeseeks que la tarea ha sido finalizada
-    sem_wait(&sem);
+    sem_wait(&sem_isFinished);
     *isFinished = 1;
-    sem_post(&sem);
+    sem_post(&sem_isFinished);
+}
+
+void informImposibleTask(){
+    sem_wait(&sem_isFinished);
+    if(*isFinished == 0){
+        printf("Rick: Looks like we are destroying this dimension with all ..burp.. the meeseeks Morty\n");
+        *isFinished = 1;
+    }
+    sem_post(&sem_isFinished);
 }
 
 void execExternalProgram(char* request){        //Ejecuta un comando que le entre como solicitud del usuario
@@ -111,12 +125,12 @@ void execExternalProgram(char* request){        //Ejecuta un comando que le entr
 }
 
 void createMrMeeseeks(int cantidad, int nivel, int type) {     //Función encargada de crear a los meeseeks
-    sem_wait(&sem);
+    sem_wait(&sem_isFinished);
     if(*isFinished == 1){       //Si la tarea fue finalizada por otro meeseek, todos los meeseeks se despiden y terminan
         printf("Byeeeeee\n");
         return;
     }
-    sem_post(&sem);
+    sem_post(&sem_isFinished);
     pid_t pId;
     printf("creando %d hijos \n", cantidad);
     for(int i = 0; i < cantidad; i++){      //crea n procesos hijos para el proceso
@@ -130,12 +144,13 @@ void createMrMeeseeks(int cantidad, int nivel, int type) {     //Función encarg
     }
     int instances;
     if(pId == 0) {  //Proceso hijo
-        sleep(5);
+        sleep(1);
         nivel = nivel + 1;
         instances = addInstance();
         printf("Hi I'm Mr Meeseeks! Look at Meeeee. (pid:%d, ppid: %d, n: %d, i: %d) \n", getpid(), getppid(), nivel, instances);
         if(type == 1){  //Si la tarea es una solicitud textual
             int difficulty = getDifficulty();
+            printf("Dificultad: %d\n", difficulty);
             srand(time(NULL) ^ (getpid()<<16));
             int prob  = rand() % 100 + 1;   //se hace un condición probabilistica, para determinar si el meeseeks logro hacer la tarea
             if( prob * 1.5 < difficulty){   //Si logro hacer la tarea se despide
@@ -150,8 +165,7 @@ void createMrMeeseeks(int cantidad, int nivel, int type) {     //Función encarg
                     cantMeeseeks = 1;
                 }
                 if(nivel >= LEVEL_LIMIT){   //si alcanza el limite de niveles, se terminan todos los meeseeks
-                    informFinish();
-                    printf("Rick: Looks like we are destroying this dimension with all ..burp.. the meeseeks Morty\n");
+                    informImposibleTask();
                     printf("Byeeeeee\n");
                     return;
                 }   
@@ -203,6 +217,7 @@ int main(){
                 }else if(difficulty < 85){
                     cantMeeseeks = 1;
                 }
+                type = 1;
                 break;
             case 2:  printf("Aqui va lo de eval\n");
                 break;
@@ -224,12 +239,14 @@ int main(){
         }
     }while(choice != 4);
     if(getpid() == box_id){     //una vez terminado el proceso box, se cierra los pipes y el semaforo
-        sem_destroy(&sem);
+        sem_destroy(&sem_shared_instances);
+        sem_destroy(&sem_isFinished);
+        sem_destroy(&sem_pipefds);
+        sem_destroy(&sem_pipefds2);
         close(pipefds[1]);
         close(pipefds[0]);
         close(pipefds2[1]);
         close(pipefds2[0]);
     }
-    
     return 0;
 }
